c2ac7c01bbe191c55ef14fb0de2b640acc9e3db5
Belaid Hafedh <belaidhafedh2@gmail.com>
2022-09-24 09:19:28 +0100

    apicheck

---
+++ src/components/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+const Home = () => {
+    return (
+        <div>
+            <h1 style={{ textAlign: "center" }}>CheckPoint API:</h1>
+            <h2 style={{ textAlign: "left" }}>Qu'est-ce que API
+        ?</h2>
+            <p style={{textDecoration:"justify"}}>En informatique, une interface de programmation d’application1 ou interface de programmation applicative2,3,4 (souvent désignée par le terme API pour Application Programming Interface) est un ensemble normalisé de classes, de méthodes, de fonctions et de constantes qui sert de façade par laquelle un logiciel offre des services à d'autres logiciels. Elle est offerte par une bibliothèque logicielle ou un service web, le plus souvent accompagnée d'une description qui spécifie comment des programmes consommateurs peuvent se servir des fonctionnalités du programme fournisseur.</p>
+        </div>
+    )
+}
+
+export default Home
